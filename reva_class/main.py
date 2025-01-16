b965d56d7d4011723339523b11611702a2ef7f9a
Crimson-Genesis <nico.zero.0x@gmail.com>
2025-01-16 08:49:55 +0530

    created many migration file in the Blog_Site project.
     .../.VSCodeCounter/2024-01-06_23-15-02/details.md  | 2507 ------------------
     .../2024-01-06_23-15-02/diff-details.md            |   15 -
     .../.VSCodeCounter/2024-01-06_23-15-02/diff.csv    |    2 -
     .../.VSCodeCounter/2024-01-06_23-15-02/diff.md     |   19 -
     .../.VSCodeCounter/2024-01-06_23-15-02/diff.txt    |   22 -
     .../.VSCodeCounter/2024-01-06_23-15-02/results.csv | 2494 ------------------
     .../2024-01-06_23-15-02/results.json               |    1 -
     .../.VSCodeCounter/2024-01-06_23-15-02/results.md  |  232 --
     .../.VSCodeCounter/2024-01-06_23-15-02/results.txt | 2727 --------------------
     Blog_Site/.vscode/settings.json                    |    3 -
     .../Blog_Site/__pycache__/settings.cpython-312.pyc |  Bin 3157 -> 2625 bytes
     .../Blog_Site/__pycache__/urls.cpython-312.pyc     |  Bin 1458 -> 820 bytes
     .../Blog_Site/__pycache__/wsgi.cpython-312.pyc     |  Bin 639 -> 413 bytes
     Blog_Site/Blog_Site/settings.py                    |    6 +-
     Blog_Site/db.postgresql                            |    0
     Blog_Site/db.sqlite3                               |  Bin 188416 -> 192512 bytes
     Blog_Site/main/__pycache__/admin.cpython-312.pyc   |  Bin 762 -> 775 bytes
     Blog_Site/main/__pycache__/forms.cpython-312.pyc   |  Bin 5248 -> 5261 bytes
     Blog_Site/main/__pycache__/models.cpython-312.pyc  |  Bin 6839 -> 6852 bytes
     Blog_Site/main/__pycache__/urls.cpython-312.pyc    |  Bin 1346 -> 1359 bytes
     Blog_Site/main/__pycache__/views.cpython-312.pyc   |  Bin 9845 -> 9864 bytes
     .../__pycache__/0001_initial.cpython-312.pyc       |  Bin 4627 -> 4640 bytes
     Blog_Site/main/urls.py                             |   11 +-
     Blog_Site/main/views.py                            |    5 +-
     Blog_Site/redme.md                                 |   17 -
     Blog_Site/templates/index.html                     |    2 +-
     Number_to_letter.py                                |    3 +-
     mechine_learn/__pycache__/function.cpython-312.pyc |  Bin 6402 -> 6400 bytes
     28 files changed, 13 insertions(+), 8053 deletions(-)

--- reva_class/main.py
+++ reva_class/main.py
@@ -1,1 +1,8 @@
-print("Hello, World!!!")
+#!/usr/bin/env python
+
+my_string = "Hello, World!"
+index_world = my_string.find("World")
+print("Index of World:- ", index_world)
+
+new_string = my_string.replace("World", "University")
+print("New String:- ", new_string)
